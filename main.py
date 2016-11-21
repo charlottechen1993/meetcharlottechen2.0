4debc871280e7b501d0ee4f89fa07c6b1aab0ec7
Charlotte Chen <charlottechen21@gmail.com>
2016-11-21 15:38:54 -0500

    ready for 1st deployment

---
+++ main.py
@@ -0,0 +1,51 @@
+import os
+import time
+import datetime
+import logging
+import webapp2
+
+from google.appengine.api import mail
+from google.appengine.api import users
+from google.appengine.ext import ndb
+from google.appengine.ext.webapp import template
+
+###############################################################################
+# retrieve and render a template.
+def render_template(handler, templatename, templatevalues={}):
+	path = os.path.join(os.path.dirname(__file__), '', templatename)
+	html = template.render(path, templatevalues)
+	handler.response.out.write(html)
+    
+###############################################################################
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        params = {
+        }
+        render_template(self, 'index.html', params)
+        
+class ContactHandler(webapp2.RequestHandler):
+    def get(self):
+        params = {
+            
+        }
+        render_template(self, 'index.html', params)
+        
+    def post(self):
+        sender_email = self.request.get('email')
+        subject = self.request.get('subject')
+        content = self.request.get('content')
+        mail.send_mail(sender="charlottechen21@gmail.com", 
+                       to="charlottechen21@gmail.com", 
+                       subject=subject, 
+                       body="Email from: " + sender_email + "\n\n" + content)
+        render_template(self, 'index.html', {})
+        self.redirect('/')
+        
+        
+###############################################################################
+mappings = [
+	('/', MainHandler),
+    ('/', ContactHandler),
+]
+app = webapp2.WSGIApplication(mappings, debug=True)
+
